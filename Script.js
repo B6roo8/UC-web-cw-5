1de387a7b3c87ad0ea6c80d0030c5bcfdde63a0f
Bo6roo8 <tareq.hussain@icloud.com>
2022-09-14 19:38:18 +0300

    Done

---
+++ Script.js
@@ -0,0 +1,26 @@
+alert('Hello I am Tareq and I will calculate your grade')
+
+let grade = prompt('Enter your Grade')
+    console.log(grade)
+    console.log('%c grade ', 'background: #222; color: #bada55');
+    
+if (grade >= 90 && grade<=100){
+    console.log('You got an amazing mark')
+}else if (grade >= 80 && grade<=89){
+    console.log('You got a excellent mark')
+}
+else if (grade >= 70 && grade<=79){
+    console.log('You got a good mark')
+}
+else if (grade >= 60 && grade<=69){
+    console.log('You got a passing grade')
+}
+else if (grade >= 50 && grade<=59){
+    console.log('You got weak mark')
+} else {
+    grade>50
+    console.log('You failed')
+}
+//Javascript is not Java
+    
+
